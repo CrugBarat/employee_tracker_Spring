b61912d3455af03193106302f7080a18c2db470f
CrugBarat <craigtbarratt@gmail.com>
2020-06-08 20:36:53 +0100

    tests multiple createAndSaves - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/com/codeclan/EmployeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/codeclan/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -23,4 +23,18 @@ class EmployeeTrackerApplicationTests {
 		employeeRepository.save(employee);
 	}
 
+	@Test
+	public void canCreateAndSaveMulitpleEmployee() {
+		Employee employee2 = new Employee("Lisa Johnstone", 21, 7348742, "lisa_j@email.com");
+		Employee employee3 = new Employee("John Smith", 22, 7278238, "john_smith@email.com");
+		Employee employee4 = new Employee("Henry Dickson", 43, 3928953, "henry_dicksen@email.com");
+		Employee employee5 = new Employee("Jennifer Robertson", 27, 983923, "jenny_rob@email.com");
+		Employee employee6 = new Employee("Susan Hughes", 38, 3292839, "susan_hughes@email.com");
+		employeeRepository.save(employee2);
+		employeeRepository.save(employee3);
+		employeeRepository.save(employee4);
+		employeeRepository.save(employee5);
+		employeeRepository.save(employee6);
+	}
+
 }
