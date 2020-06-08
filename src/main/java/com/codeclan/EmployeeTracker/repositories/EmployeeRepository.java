cd9c82005ea4dddc9b3dac08f0a07d3c4697211c
CrugBarat <craigtbarratt@gmail.com>
2020-06-08 20:33:42 +0100

    extends JPARepo to EmployeeRepo interface
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/com/codeclan/EmployeeTracker/repositories/EmployeeRepository.java
+++ src/main/java/com/codeclan/EmployeeTracker/repositories/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.codeclan.EmployeeTracker.repositories;
+import com.codeclan.EmployeeTracker.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
