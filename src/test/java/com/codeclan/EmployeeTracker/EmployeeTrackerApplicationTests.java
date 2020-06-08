ad677386e5d1b36ec600b930e89f09df050c7234
CrugBarat <craigtbarratt@gmail.com>
2020-06-08 20:35:58 +0100

    seeds Db and tests createAndSaveEmployee - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/com/codeclan/EmployeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/codeclan/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -1,1 +1,26 @@
+package com.codeclan.EmployeeTracker;
+import com.codeclan.EmployeeTracker.models.Employee;
+import com.codeclan.EmployeeTracker.repositories.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class EmployeeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Test
+	public void canCreateAndSaveEmployee() {
+		Employee employee = new Employee("Jane Doe", 35, 1234567, "jane_doe@email.com");
+		employeeRepository.save(employee);
+	}
+
+}
