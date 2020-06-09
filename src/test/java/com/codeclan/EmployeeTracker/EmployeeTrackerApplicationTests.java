dd4341a006799fa2f1c9e915693f75cd1b0de1a0
CrugBarat <craigtbarratt@gmail.com>
2020-06-09 13:30:27 +0100

    adds test to create employees/departments/projects = APP ERROR
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/com/codeclan/EmployeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/codeclan/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -1,8 +1,10 @@
 package com.codeclan.EmployeeTracker;
 import com.codeclan.EmployeeTracker.models.Department;
 import com.codeclan.EmployeeTracker.models.Employee;
+import com.codeclan.EmployeeTracker.models.Project;
 import com.codeclan.EmployeeTracker.repositories.DepartmentRepository;
 import com.codeclan.EmployeeTracker.repositories.EmployeeRepository;
+import com.codeclan.EmployeeTracker.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,17 +18,35 @@ public class EmployeeTrackerApplicationTests {
 	@Autowired
 	private DepartmentRepository departmentRepository;
 
+	@Autowired
+	private ProjectRepository projectRepository;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 	@Test
-	public void createEmployeeAndDepartment(){
+	public void createEmployeeAndDepartment() {
 		Department department = new Department("Finance");
 		departmentRepository.save(department);
 
 		Employee employee = new Employee("Jane Doe", 30, 127733, "jane_doe@email.com", department);
 		employeeRepository.save(employee);
+	}
+
+	@Test
+	public void addEmployeesAndProjects() {
+		Department department = new Department("Finance");
+		departmentRepository.save(department);
+
+		Employee employee = new Employee("Jane Doe", 30, 127733, "jane_doe@email.com", department);
+		employeeRepository.save(employee);
+
+		Project project = new Project("Public Housing Crisis", 14);
+		projectRepository.save(project);
+
+		project.addEmployee(employee);
+		projectRepository.save(project);
 	}
 
 }
