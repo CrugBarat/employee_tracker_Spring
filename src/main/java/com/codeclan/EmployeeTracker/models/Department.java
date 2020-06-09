1c2d5514dcec98406c3ed256f44d2188a10411c8
CrugBarat <craigtbarratt@gmail.com>
2020-06-09 12:37:47 +0100

    adds Department POJO to models with annotation/constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/com/codeclan/EmployeeTracker/models/Department.java
@@ -0,0 +1,51 @@
+package com.codeclan.EmployeeTracker.models;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "departments")
+    private List<Employee> employees;
+
+    public Department(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public Department() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
